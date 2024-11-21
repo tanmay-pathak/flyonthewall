1303936212c3db1f911ea13afba8ac04f196aa8d
Hassan El Mghari <hassan4709@gmail.com>
2024-11-21 11:54:06 -0500

    made homepage a little nicer

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## TODOs
 
-- [ ] Add a loading state when things are loading
-- [ ] Add some guidelines for best pics
 - [ ] Slight design changes to main page to make it more professional
+- [ ] Add a loading state when things are loading
 - [ ] Be able to click on the image to see additional details (ingredients, origin, taste)
+- [ ] Add some guidelines for best pics
--- app/page.tsx
+++ app/page.tsx
@@ -53,12 +53,21 @@ export default function Home() {
   );
 
   return (
-    <div className="container mx-auto px-4 py-8 bg-white">
-      <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">
-        Upload Your Menu
-      </h1>
+    <div className="container text-center px-4 py-8 bg-white max-w-5xl mx-auto">
+      <div className="max-w-2xl text-center mx-auto mt-20">
+        <p className="mx-auto mb-5 w-fit rounded-2xl border px-4 py-1 text-sm text-slate-500">
+          <b>100+</b> menus uploaded so far
+        </p>
+        <h1 className="mb-6 text-balance text-6xl font-bold text-zinc-800">
+          Visualize your menu with AI
+        </h1>
+        <p className="mb-8 text-lg text-gray-500">
+          Take a picture of your menu and get nice pictures of each dish so you
+          can better decide what to order.
+        </p>
+      </div>
 
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-2xl mx-auto">
         {status === "initial" && (
           <Dropzone
             multiple={false}
@@ -84,7 +93,7 @@ export default function Home() {
                     >
                       <p className="text-xl">Upload your menu</p>
                       <p className="mt-1 font-normal text-gray-600">
-                        or drag and drop
+                        or take a picture
                       </p>
                     </label>
                   </div>
@@ -102,30 +111,29 @@ export default function Home() {
               height={768}
               src={menuUrl}
               alt="Menu"
-              className="w-80 rounded-lg shadow-md"
+              className="w-40 rounded-lg shadow-md"
             />
           </div>
         )}
-
-        {parsedMenu.length > 0 && (
-          <div className="mt-10">
-            <h2 className="text-2xl font-bold mb-5">
-              Menu – {parsedMenu.length} dishes detected
-            </h2>
-            <div className="relative mb-6">
-              <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
-              <Input
-                type="text"
-                placeholder="Search menu items..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="pl-10"
-              />
-            </div>
-            <MenuGrid items={filteredMenu} />
-          </div>
-        )}
       </div>
+      {parsedMenu.length > 0 && (
+        <div className="mt-10">
+          <h2 className="text-4xl font-bold mb-5">
+            Menu – {parsedMenu.length} dishes detected
+          </h2>
+          <div className="relative mb-6">
+            <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
+            <Input
+              type="text"
+              placeholder="Search menu items..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="pl-10"
+            />
+          </div>
+          <MenuGrid items={filteredMenu} />
+        </div>
+      )}
     </div>
   );
 }
