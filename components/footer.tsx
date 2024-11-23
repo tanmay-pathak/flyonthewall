c01abaf5ca89b1a07262dd36e995a342c8cd210f
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2024-11-23 15:13:42 -0600

    refactor: cleanup

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,3 @@
-import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
-
 export function Footer() {
   return (
     <footer className="bg-white border-t border-zuPrimary mt-auto">
@@ -7,16 +5,7 @@ export function Footer() {
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="text-center md:text-left mb-4 md:mb-0">
             <p className="text-gray-500">
-              Powered by{" "}
-              <a
-                href="https://dub.sh/together-ai"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-500 hover:text-gray-900 transition-colors  underline-offset-4 underline"
-              >
-                Together AI
-              </a>
-              . Created by Jasmine, Jon, Max, and Tanmay.
+              Created by Jasmine, Jon, Max, and Tanmay.
             </p>
           </div>
         </div>
--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,26 @@
+"use client";
+
 import Link from "next/link";
 
 export function Header() {
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
   return (
     <header className="bg-zuPrimary border-b border-gray-200">
       <div className="container mx-auto px-8 py-8">
         <div className="flex items-center justify-between">
-          <Link href="/" className="flex items-center space-x-8">
-            <img src="/zu-logo.png" alt="zu" className="h-12 w-12"/>
+          <Link
+            href="/"
+            className="flex items-center space-x-8"
+            onClick={handleRefresh}
+          >
+            <img src="/zu-logo.png" alt="zu" className="h-12 w-12" />
             <span className="h-12 border-r-2 border-black"></span>
-            <span className="text-lg font-bold uppercase italic">Fly on the Wall</span>
+            <span className="text-lg font-bold uppercase italic">
+              Fly on the Wall
+            </span>
           </Link>
         </div>
       </div>
